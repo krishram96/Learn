03eeb3826bfbaf5d21503267db1a3f042dd3c097
krishna kumar <krishram205@gmail.com>
2026-02-24 13:20:08 +0530

    Add practice.py

---
+++ practice.py
@@ -0,0 +1,12 @@
+employees =[
+    {"name":"krishna","salary":1000},
+    {"name":"meena","salary":2000}
+]
+
+high_salary=[]
+
+for emp in employees:
+    if emp["salary"]>1000:
+        high_salary.append(emp)
+
+print(high_salary)
